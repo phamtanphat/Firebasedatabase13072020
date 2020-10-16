f3597ba56390a209a7a6669f1e985842b21852e7
phatdroid94 <phatdroid94@gmail.com>
2020-10-16 18:44:06 +0700

    object

--- app/src/main/java/com/example/firebasedatabase13072020/MainActivity.java
+++ app/src/main/java/com/example/firebasedatabase13072020/MainActivity.java
@@ -11,6 +11,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -21,7 +24,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // 1 : Lưu dữ liệu dạng string
-        myRef.child("Trungtam").setValue("Khoa Phạm Training").addOnCompleteListener(new OnCompleteListener<Void>() {
+//        myRef.child("Trungtam").setValue("Khoa Phạm Training").addOnCompleteListener(new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                if (task.isSuccessful()){
+//                    Toast.makeText(MainActivity.this, "Thêm thành công", Toast.LENGTH_SHORT).show();
+//                }else{
+//                    Toast.makeText(MainActivity.this, "Thất bại", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+        // 2 : Lưu dạng List
+//        List<String> listNames = new ArrayList<>();
+//        listNames.add("Nguyen Van A");
+//        listNames.add("Nguyen Van B");
+//        listNames.add("Nguyen Van C");
+//        listNames.add("Nguyen Van D");
+//
+//        myRef.child("Listnames").setValue(listNames).addOnCompleteListener(new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                if (task.isSuccessful()){
+//                    Toast.makeText(MainActivity.this, "Thêm thành công", Toast.LENGTH_SHORT).show();
+//                }else{
+//                    Toast.makeText(MainActivity.this, "Thất bại", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+        // 3 : Lưu dạng object
+        Vehicle bicycle = new Vehicle("Bicycle",2);
+        myRef.child("Vehicle").setValue(bicycle).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 if (task.isSuccessful()){
---
+++ app/src/main/java/com/example/firebasedatabase13072020/Vehicle.java
@@ -0,0 +1,14 @@
+package com.example.firebasedatabase13072020;
+
+public class Vehicle {
+    public String name;
+    public Integer numwheel;
+
+    public Vehicle(String name, Integer numwheel) {
+        this.name = name;
+        this.numwheel = numwheel;
+    }
+
+    public Vehicle() {
+    }
+}
