dddaeefa4ef0910081100c7ea2264d653dcf2af0
phatdroid94 <phatdroid94@gmail.com>
2020-10-16 19:10:32 +0700

    lay du lieu dang list

--- app/src/main/java/com/example/firebasedatabase13072020/MainActivity.java
+++ app/src/main/java/com/example/firebasedatabase13072020/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.firebasedatabase13072020;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -64,9 +70,53 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
         // 4 : Lưu dạng mảng object
-        myRef.child("Course").push().setValue(new Course("Lập trình Android","Java,Kotlin"));
-        myRef.child("Course").push().setValue(new Course("Lập trình Php","Php"));
-        myRef.child("Course").push().setValue(new Course("Lập trình Ios","Swift"));
+//        myRef.child("Course").push().setValue(new Course("Lập trình Android","Java,Kotlin"));
+//        myRef.child("Course").push().setValue(new Course("Lập trình Php","Php"));
+//        myRef.child("Course").push().setValue(new Course("Lập trình Ios","Swift"));
 
+
+        // Đọc dữ liệu
+        // 1 : Dạng String
+//        myRef.child("Trungtam").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                String value = (String) snapshot.getValue();
+//                Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+        //2 : Dạng list
+        myRef.child("Listnames").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String value = (String) snapshot.getValue();
+                Log.d("BBB",value);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String value = (String) snapshot.getValue();
+                Log.d("BBB",value);
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
